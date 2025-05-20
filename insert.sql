d3c20a737a5654088e9414d91c7dd7cabb481396
Paulo Kaique Caldeira Maia <200359043+KaiqueFecaf@users.noreply.github.com>
2025-05-20 20:33:51 -0300

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO poluentes (nomePoluentes) VALUES ('CO (Monóxido de Carbono)');
+INSERT INTO poluentes (nomePoluentes) VALUES ('MP10 (Partículas Inaláveis)');
+INSERT INTO poluentes (nomePoluentes) VALUES ('MP2.5 (Partículas Inaláveis Finas)');
+INSERT INTO poluentes (nomePoluentes) VALUES ('NO (Monóxido de Nitrogênio)');
+INSERT INTO poluentes (nomePoluentes) VALUES ('NO2 (Dióxido de Nitrogênio)');
+INSERT INTO poluentes (nomePoluentes) VALUES ('NOx (Óxidos de Nitrogênio)');
+INSERT INTO poluentes (nomePoluentes) VALUES ('TEMP (Temperatura do Ar)');
+INSERT INTO poluentes (nomePoluentes) VALUES ('UR (Umidade Relativa do Ar)');
